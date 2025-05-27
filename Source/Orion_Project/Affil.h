d5fe97576312d328fc6e29942448ac06a0b2f0ac
Gian Alfonso <93459007+Imanoodle13@users.noreply.github.com>
2025-05-27 00:14:28 -0500

    ERROR: Project could not be compiled

---
+++ Source/Orion_Project/Affil.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Affil.generated.h"
+
+UENUM(BlueprintType)             // Exposes the enum to UE's reflection system, making it accessible in Blueprints.
+enum class EAffiliation : uint8 // 'uint8' is required for Blueprint compatibility
+{
+	None	UMETA(DisplayName = "None"),
+	Trojan	UMETA(DisplayName = "Trojan"),
+	Orion	UMETA(DisplayName = "Orion")
+};
