5e64452d3f6f8ff3501f629e9dfcec591db55436
Gian Alfonso <alfonsogian225@gmail.com>
2025-12-09 22:02:12 -0600

    Implemented child class: Station of ControlPoint

---
+++ Source/Orion_Project/Private/Station.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Station.h"
+
+AStation::AStation()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Default values
+	boostProductionScale = 1.5f;
+}
+
+void AStation::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AStation::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
